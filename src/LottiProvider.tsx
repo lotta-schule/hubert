f0020f6f0fd9fcead9522b38eb3f968523a36b91
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-07-03 18:39:06 +0200

    LottiProvider: Only try to add dialogContainer-div when in Browser environment

--- src/LottiProvider.tsx
+++ src/LottiProvider.tsx
@@ -4,11 +4,12 @@ import { ThemeProvider, ThemeProviderProps, GlobalStyles } from './theme';
 
 export type LottiProviderProps = ThemeProviderProps;
 export const LottiProvider = ({ theme, children }: LottiProviderProps) => {
+  const isBrowser = typeof window !== 'undefined';
   const dialogContainerRef = React.useRef(
-    document.getElementById('dialogcontainer')
+    isBrowser ? document.getElementById('dialogcontainer') : null
   );
 
-  if (!dialogContainerRef.current) {
+  if (isBrowser && !dialogContainerRef.current) {
     const dialogContainer = document.createElement('div');
     dialogContainer.id = 'dialogContainer';
     document.body.appendChild(dialogContainer);
