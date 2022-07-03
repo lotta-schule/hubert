c181b162cf7d8f2d1da6dd74053d5c64f2f234f5
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-07-03 15:53:58 +0200

    Add some console.log to github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,27 @@ import viteReactPlugin from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 export default Vite.defineConfig({
-    plugins: [
-        viteReactPlugin(),
-        dts({
-            insertTypesEntry: true,
-        }),
-    ],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'lotti',
-            formats: ['es', 'umd'],
-            fileName: (format) => `lotti.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                },
-            },
-        },
+  plugins: [
+    viteReactPlugin(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'lotti',
+      formats: ['es', 'umd'],
+      fileName: (format) => `lotti.${format}.js`,
     },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
