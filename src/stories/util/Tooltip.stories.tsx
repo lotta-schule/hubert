0a7b814739c8e4ba7bc9a85a61dbaf0689c0884f
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-07-10 10:09:44 +0200

    Replace @reach/tooltip

---
+++ src/stories/util/Tooltip.stories.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { Tooltip } from '../../util';
+import { Button } from '../../button';
+
+export default {
+  title: 'util/Tooltip',
+  component: Tooltip,
+} as Meta;
+
+const Template: Story = (args: any) => <Tooltip {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: <Button label={'Hover me'} />,
+  label: 'Simple Tooltip',
+};
+
+export const Image = Template.bind({});
+Image.args = {
+  children: <Button label={'Hover me'} />,
+  label: (
+    <img
+      src="https://picsum.photos/600/400"
+      alt="Wahrscheinlich wunderschönes Foto"
+    />
+  ),
+};
--- src/util/Tooltip.tsx
+++ src/util/Tooltip.tsx
@@ -1,28 +1,69 @@
 import * as React from 'react';
-import { Tooltip as ReachTooltip } from '@reach/tooltip';
+import { useTooltip, useTooltipTrigger } from '@react-aria/tooltip';
+import {
+  useTooltipTriggerState,
+  TooltipTriggerState,
+} from '@react-stately/tooltip';
+import { mergeProps } from '@react-aria/utils';
 import clsx from 'clsx';
 
 import styles from './Tooltip.module.scss';
 
 export interface TooltipProps extends React.HTMLAttributes<HTMLDivElement> {
-  label: React.ReactNode;
+  children: React.ReactElement;
+  label?: string | React.ReactElement<{}>;
 }
 
 export const Tooltip = React.forwardRef(
   (
-    { children, label, className, ...props }: TooltipProps,
-    ref: React.ForwardedRef<HTMLDivElement>
+    { children, className, label, ...props }: TooltipProps,
+    refProp: React.ForwardedRef<HTMLDivElement | null>
   ) => {
+    const state = useTooltipTriggerState({});
+    const ref = React.useRef<HTMLDivElement | null>(
+      (refProp as React.MutableRefObject<HTMLDivElement>)?.current ?? null
+    );
+
+    let { triggerProps, tooltipProps } = useTooltipTrigger({}, state, ref);
+
     return (
-      <ReachTooltip
-        ref={ref}
-        label={label}
-        className={clsx(styles.root, className)}
-        {...props}
-      >
-        {children}
-      </ReachTooltip>
+      <div className={styles.root}>
+        {React.cloneElement(children, { ref, ...triggerProps })}
+        <TooltipOverlay
+          className={className}
+          state={state}
+          label={label ?? children}
+          {...mergeProps(props, tooltipProps)}
+        />
+      </div>
     );
   }
 );
 Tooltip.displayName = 'Tooltip';
+
+export interface TooltipOverlayProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  label?: string | React.ReactElement<{}>;
+  state: TooltipTriggerState;
+}
+
+export const TooltipOverlay = React.memo(
+  React.forwardRef(
+    (
+      { state, label, className, ...props }: TooltipOverlayProps,
+      ref: React.ForwardedRef<HTMLDivElement>
+    ) => {
+      const { tooltipProps } = useTooltip(props, state);
+      return state.isOpen ? (
+        <div
+          className={clsx(className, styles.tooltip)}
+          ref={ref}
+          {...mergeProps(props, tooltipProps)}
+        >
+          {label}
+        </div>
+      ) : null;
+    }
+  )
+);
+TooltipOverlay.displayName = 'TooltipOverlay';
