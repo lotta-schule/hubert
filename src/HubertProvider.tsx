4ef335b2b8eff8330a7e375d0efb55fbaf864072
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-07-15 07:15:53 +0200

    Add SSRProvider

--- src/HubertProvider.tsx
+++ src/HubertProvider.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { OverlayProvider } from '@react-aria/overlays';
 import { ThemeProvider, ThemeProviderProps, GlobalStyles } from './theme';
+import { SSRProvider } from '@react-aria/ssr';
 
 export type HubertProviderProps = ThemeProviderProps;
 export const HubertProvider = ({ theme, children }: HubertProviderProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <OverlayProvider>{children}</OverlayProvider>
-    </ThemeProvider>
+    <SSRProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <OverlayProvider>{children}</OverlayProvider>
+      </ThemeProvider>
+    </SSRProvider>
   );
 };
