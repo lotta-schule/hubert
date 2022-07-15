ab3c2b05681419b697fbc1ca764fc2496570e57b
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-07-15 06:30:09 +0200

    new Tooltip test

--- src/util/tooltip/Tooltip.test.tsx
+++ src/util/tooltip/Tooltip.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { Button } from '../../button/Button';
 import { Tooltip } from './Tooltip';
+import userEvent from '@testing-library/user-event';
 
 describe('util/Tooltip', () => {
   it('should render', () => {
@@ -10,5 +11,16 @@ describe('util/Tooltip', () => {
       </Tooltip>
     );
     expect(screen.container).toMatchSnapshot();
+  });
+
+  it('should keep a button clickable', () => {
+    const onClick = jest.fn();
+    const screen = render(
+      <Tooltip label="Test">
+        <Button onClick={onClick}>Test</Button>
+      </Tooltip>
+    );
+    userEvent.click(screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalled();
   });
 });
