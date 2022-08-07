4455aa66b4031f155085db7db6091fb87b343480
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-08-07 19:08:13 +0200

    fix static build dir

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,29 @@
+const { mergeConfig } = require('vite');
+
 module.exports = {
-    stories: [
-        '../src/**/*.stories.mdx',
-        '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        '@storybook/addon-a11y',
-    ],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-vite',
-    },
-    features: {
-        storyStoreV7: true,
-    },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      const newConfig = mergeConfig(config, {
+        base: './',
+      });
+      console.log(newConfig);
+      return newConfig;
+    }
+
+    return config;
+  },
 };
