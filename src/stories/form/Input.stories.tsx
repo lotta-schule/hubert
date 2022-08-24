bf0d1a1537f11dfea127e2d7144f54ce47c90cc2
Alexis Rinaldoni <ptitmouton@users.noreply.github.com>
2022-08-24 20:05:22 +0200

    add interactions (#4)

--- src/stories/form/Input.stories.tsx
+++ src/stories/form/Input.stories.tsx
@@ -1,28 +1,51 @@
 import * as React from 'react';
-import { Story, Meta } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { within } from '@testing-library/react';
+import { userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { Input } from '../../form';
 
 export default {
   title: 'Form/Input',
   component: Input,
   argTypes: {},
-} as Meta;
+} as ComponentMeta<typeof Input>;
 
-const Template: Story = (args) => <Input {...args} />;
-
-export const Default = Template.bind({});
+export const Default: ComponentStory<typeof Input> = (args) => (
+  <Input {...args} />
+);
 Default.args = {
   placeholder: 'Please type something interesting ...',
 };
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
 
-export const Inline = Template.bind({});
+  userEvent.click(canvas.getByRole('textbox'));
+  await userEvent.keyboard('sample text', { delay: 100 });
+
+  expect(canvas.getByRole('textbox')).toHaveValue('sample text');
+};
+
+export const Inline: ComponentStory<typeof Input> = (args) => (
+  <Input {...args} />
+);
 Inline.args = {
   ...Default.args,
   inline: true,
 };
 
-export const Multiline = Template.bind({});
+export const Multiline: ComponentStory<typeof Input> = (args) => (
+  <Input {...args} />
+);
 Multiline.args = {
   ...Default.args,
   multiline: true,
+} as any;
+Multiline.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  userEvent.click(canvas.getByRole('textbox'));
+  await userEvent.keyboard('sample text\nwith newline', { delay: 100 });
+
+  expect(canvas.getByRole('textbox')).toHaveValue('sample text\nwith newline');
 };
