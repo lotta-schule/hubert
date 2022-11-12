d66e3f8a02bf6fff0b3a42d10b2a3063cd8e423d
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-11-12 10:02:04 +0100

    fix GlobalStyles type error

--- src/theme/GlobalStyles.tsx
+++ src/theme/GlobalStyles.tsx
@@ -26,55 +26,57 @@ export type GlobalStylesProps = {
   supportedFonts?: ExternalFont[];
 };
 
-export const GlobalStyles = React.memo(({ supportedFonts = [] }) => {
-  const theme = useTheme();
+export const GlobalStyles = React.memo(
+  ({ supportedFonts = [] }: GlobalStylesProps) => {
+    const theme = useTheme();
 
-  const getVarValue = (value: string) => {
-    try {
-      const color = new Color(value);
-      const { r, g, b } = color.srgb;
-      return [r, g, b].map((c) => (c * 255).toFixed(0)).join(', ');
-    } catch (e) {
-      return value;
-    }
-  };
-
-  const getVarListWithName = (
-    key: string,
-    value: Record<string, any>
-  ): [varname: string, varval: string][] => {
-    return Object.keys(value).reduce((acc, valueKey) => {
-      const varKey = [key, valueKey].filter(Boolean).map(kebabCase).join('-');
-      const valueValue = value[valueKey];
-      if (typeof valueValue === 'object') {
-        return acc.concat(getVarListWithName(varKey, valueValue));
+    const getVarValue = (value: string) => {
+      try {
+        const color = new Color(value);
+        const { r, g, b } = color.srgb;
+        return [r, g, b].map((c) => (c * 255).toFixed(0)).join(', ');
+      } catch (e) {
+        return value;
       }
-      return [...acc, [varKey, getVarValue(valueValue)]];
-    }, [] as [string, string][]);
-  };
+    };
 
-  React.useEffect(() => {
-    for (const [varKey, varVal] of getVarListWithName('', theme)) {
-      document.documentElement.style.setProperty(`--lotta-${varKey}`, varVal);
+    const getVarListWithName = (
+      key: string,
+      value: Record<string, any>
+    ): [varname: string, varval: string][] => {
+      return Object.keys(value).reduce((acc, valueKey) => {
+        const varKey = [key, valueKey].filter(Boolean).map(kebabCase).join('-');
+        const valueValue = value[valueKey];
+        if (typeof valueValue === 'object') {
+          return acc.concat(getVarListWithName(varKey, valueValue));
+        }
+        return [...acc, [varKey, getVarValue(valueValue)]];
+      }, [] as [string, string][]);
+    };
 
-      if (varKey.endsWith('-font-family')) {
-        const fontName = varVal.match("'(.+)'")?.[1] ?? varVal;
-        const fontDef = supportedFonts.find((f) => f.name === fontName);
-        if (fontName && fontDef) {
-          if (
-            !document.head.querySelector(`link[data-font-name="${fontName}"]`)
-          ) {
-            const link = document.createElement('link');
-            link.setAttribute('rel', 'stylesheet');
-            link.setAttribute('href', fontDef.url);
-            link.setAttribute('data-font-name', fontName);
-            document.head.appendChild(link);
+    React.useEffect(() => {
+      for (const [varKey, varVal] of getVarListWithName('', theme)) {
+        document.documentElement.style.setProperty(`--lotta-${varKey}`, varVal);
+
+        if (varKey.endsWith('-font-family')) {
+          const fontName = varVal.match("'(.+)'")?.[1] ?? varVal;
+          const fontDef = supportedFonts.find((f) => f.name === fontName);
+          if (fontName && fontDef) {
+            if (
+              !document.head.querySelector(`link[data-font-name="${fontName}"]`)
+            ) {
+              const link = document.createElement('link');
+              link.setAttribute('rel', 'stylesheet');
+              link.setAttribute('href', fontDef.url);
+              link.setAttribute('data-font-name', fontName);
+              document.head.appendChild(link);
+            }
           }
         }
       }
-    }
-  }, [theme]);
+    }, [theme]);
 
-  return null;
-});
+    return null;
+  }
+);
 GlobalStyles.displayName = 'GlobalHubertStyles';
