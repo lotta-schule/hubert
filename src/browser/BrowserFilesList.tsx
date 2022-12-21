3adbd92419ed62dde396453b00adabe6bcf8737f
Christopher Bill <cb.billy.94@gmail.com>
2022-12-21 21:24:01 +0100

    browser layout columns

--- src/browser/BrowserFilesList.tsx
+++ src/browser/BrowserFilesList.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import {
   faFileImage,
   faFilePdf,
@@ -5,142 +6,148 @@ import {
   faPen,
   faTrashCan,
 } from '@fortawesome/free-solid-svg-icons';
-import * as React from 'react';
 import { Icon } from '../icon';
+import clsx from 'clsx';
 
 import styles from './BrowserFilesList.module.scss';
 
-export const BrowserFilesList = React.memo(() => {
-  return (
-    <ul className={styles.root}>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileImage} />
-        </div>
-        <div className={styles.fileName}>Dateiname 1</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-          <Icon icon={faTrashCan} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileImage} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFilePdf} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFilePdf} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileImage} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileImage} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFilePdf} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFilePdf} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileWord} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileWord} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileWord} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileWord} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileImage} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-      <li>
-        <div className={styles.fileIcon}>
-          <Icon icon={faFileImage} />
-        </div>
-        <div className={styles.fileName}>Dateiname 2</div>
-        <div className={styles.fileEdit}>
-          <Icon icon={faPen} />
-        </div>
-      </li>
-    </ul>
-  );
-});
+export type BrowserFilesListProps = {
+  narrow?: boolean;
+};
+
+export const BrowserFilesList = React.memo(
+  ({ narrow }: BrowserFilesListProps) => {
+    return (
+      <ul className={clsx(styles.root, { [styles.narrow]: narrow })}>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileImage} />
+          </div>
+          <div className={styles.fileName}>Dateiname 1</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+            <Icon icon={faTrashCan} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileImage} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFilePdf} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFilePdf} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileImage} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileImage} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFilePdf} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFilePdf} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileWord} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileWord} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileWord} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileWord} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileImage} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+        <li>
+          <div className={styles.fileIcon}>
+            <Icon icon={faFileImage} />
+          </div>
+          <div className={styles.fileName}>Dateiname 2</div>
+          <div className={styles.fileEdit}>
+            <Icon icon={faPen} />
+          </div>
+        </li>
+      </ul>
+    );
+  }
+);
 BrowserFilesList.displayName = 'BrowserFilesList';
--- src/browser/BrowserFilesWindow.tsx
+++ src/browser/BrowserFilesWindow.tsx
@@ -5,14 +5,26 @@ import { BrowserFilePreview } from './BrowserFilePreview';
 import styles from './BrowserFilesWindow.module.scss';
 
 export const BrowserFilesWindow = React.memo(() => {
+  const [isShowPreview, setIsShowPreview] = React.useState(false);
   return (
-    <div className={styles.root}>
-      <div className={styles.BrowserColumns}>
-        <BrowserFilesList />
-        <BrowserFilesList />
-        <BrowserFilesList />
+    <div>
+      <input
+        type={'checkbox'}
+        checked={isShowPreview}
+        onChange={(e) => {
+          setIsShowPreview(e.currentTarget.checked);
+        }}
+      />{' '}
+      Vorschau anzeigen
+      <div className={styles.root}>
+        <div className={styles.BrowserColumns}>
+          <BrowserFilesList narrow={!isShowPreview} />
+          <BrowserFilesList narrow={!isShowPreview} />
+          <BrowserFilesList narrow={!isShowPreview} />
+          <BrowserFilesList narrow={!isShowPreview} />
+        </div>
+        {isShowPreview && <BrowserFilePreview />}
       </div>
-      <BrowserFilePreview />
     </div>
   );
 });
