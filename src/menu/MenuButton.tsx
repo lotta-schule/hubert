d120fe59b9a876e87db821bc4624fdf90b809ff8
Christopher Bill <cb.billy.94@gmail.com>
2023-05-03 20:47:18 +0200

    Remove root element from MenuButton

--- src/menu/MenuButton.tsx
+++ src/menu/MenuButton.tsx
@@ -13,8 +13,6 @@ import styles from './MenuButton.module.scss';
 
 export type MenuButtonProps = {
   buttonProps: Omit<ButtonProps, 'ref'>;
-  className?: string;
-  style?: React.CSSProperties;
   children: CollectionChildren<object>;
   placement?: PopoverProps['placement'];
   onOpenChange?: (_isOpen: boolean) => void;
@@ -23,14 +21,7 @@ export type MenuButtonProps = {
 
 export const MenuButton = React.forwardRef(
   (
-    {
-      buttonProps,
-      className,
-      onOpenChange,
-      placement,
-      style,
-      ...props
-    }: MenuButtonProps,
+    { buttonProps, onOpenChange, placement, ...props }: MenuButtonProps,
     forwardedRef: React.Ref<HTMLButtonElement | null>
   ) => {
     const isBrowser = typeof window !== 'undefined';
@@ -67,7 +58,7 @@ export const MenuButton = React.forwardRef(
     }
 
     return (
-      <div className={clsx(styles.root, className)} style={style}>
+      <>
         <Button ref={ref} {...buttonProps} {...ariaButtonProps} />
         {ref.current && (
           <Popover
@@ -88,7 +79,7 @@ export const MenuButton = React.forwardRef(
             </Menu>
           </Popover>
         )}
-      </div>
+      </>
     );
   }
 );
