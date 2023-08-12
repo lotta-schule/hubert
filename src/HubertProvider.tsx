05cadf81985448d9eac050982b2ee6cf20433732
Alexis Rinaldoni <arinaldoni@me.com>
2023-08-12 10:12:45 +0200

    Remove SSRProvider completly

--- src/HubertProvider.tsx
+++ src/HubertProvider.tsx
@@ -1,18 +1,10 @@
 import * as React from 'react';
 import { OverlayProvider } from '@react-aria/overlays';
-import { SSRProvider } from '@react-aria/ssr';
 
 export type HubertProviderProps = {
   children?: React.ReactNode;
 };
 
 export const HubertProvider = ({ children }: HubertProviderProps) => {
-  if (React.version.startsWith('18')) {
-    return <OverlayProvider>{children}</OverlayProvider>;
-  }
-  return (
-    <SSRProvider>
-      <OverlayProvider>{children}</OverlayProvider>
-    </SSRProvider>
-  );
+  return <OverlayProvider>{children}</OverlayProvider>;
 };
