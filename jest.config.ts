635f1817da7c7a900be936bc42e987f1942027be
Alexis Rinaldoni <arinaldoni@me.com>
2023-08-13 16:57:14 +0200

    workflows: exec interaction tests on storybook, split up workflows

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: './coverage/jest',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
@@ -36,7 +36,7 @@ export default {
   ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
